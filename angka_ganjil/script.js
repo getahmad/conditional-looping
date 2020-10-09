958811dc3cefd0667d80a23eb2b8370877a72413
Ahmad Danang <ahmad.danang91@gmail.com>
2020-10-09 20:50:41 +0700

    penambahan koma

--- angka_ganjil/script.js
+++ angka_ganjil/script.js
@@ -2,7 +2,7 @@ alert ("Rules, silahkan masukkan angka untuk menampilkan angka ganjil")
 let angka = prompt ("masukkan angka: ")
 for (let angkaGanjil = 0; angkaGanjil <= angka; angkaGanjil ++ ){
     if (angkaGanjil % 2 === 1){
-        document.write(` ${angkaGanjil}`)
+        document.write(`${angkaGanjil}, `)
     }else{
     }
 }
--- angka_genap/script.js
+++ angka_genap/script.js
@@ -3,7 +3,7 @@ alert ("Rules, silahkan masukkan angka untuk menampilkan angka genap")
 let angka = prompt ("masukkan angka: ")
 for (let angkaGenap = 0; angkaGenap <= angka; angkaGenap ++ ){
     if (angkaGenap % 2 === 0){
-        document.write(` ${angkaGenap}`)
+        document.write(`${angkaGenap}, `)
     }else{
     }
 }
